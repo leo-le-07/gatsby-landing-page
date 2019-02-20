096cd8248072ee9004be78e95c4cb11e0848e5c9
Leo <coder.leo.le@gmail.com>
2019-02-20 15:13:22 +0700

    Initial

--- src/components/Navi/index.js
+++ src/components/Navi/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import './style.scss'
 
 class Navi extends React.Component {
   render() {
     const { location, title } = this.props
     return (
-      <nav className="navbar navbar-expand navbar-dark flex-column flex-md-row bg-primary">
+      <nav className="navbar navbar-expand navbar-dark flex-column flex-md-row header">
         <div className="container">
-          <Link className="text-center" to="/">
-            <h1 className="navbar-brand mb-0">{title}</h1>
+          <Link className="text-center navbar-brand mb-0" to="/">
+            <img
+              src="http://pocoexpress.com.vn/wp-content/uploads/2016/05/xlogo-poco.png.pagespeed.ic.aIMUc3KO3L.webp"
+              alt="Sea Freight, Air Freight, Multimodal transport, Logistics services"
+            />
           </Link>
           <div className="navbar-nav-scroll">
             <ul className="navbar-nav bd-navbar-nav flex-row">
@@ -23,12 +27,12 @@ class Navi extends React.Component {
               </li>
               <li
                 className={
-                  location.pathname === '/profile/'
+                  location.pathname === '/profile'
                     ? 'nav-item active'
                     : 'nav-item'
                 }
               >
-                <Link to="/profile/" className="nav-link">
+                <Link to="/profile" className="nav-link">
                   Profile
                 </Link>
               </li>
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,15 +11,40 @@ const BlogIndex = ({ data, location }) => {
   return (
     <Layout location={location}>
       <Meta site={get(data, 'site.meta')} />
-      {posts.map(({ post }, i) => (
-        <Post
-          data={post}
-          options={{
-            isIndex: true,
-          }}
-          key={i}
-        />
-      ))}
+      <header className="masthead text-white text-center">
+        <div className="overlay" />
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-9 mx-auto">
+              <h1 className="mb-5">
+                Build a landing page for your business or project and generate
+                more leads!
+              </h1>
+            </div>
+            <div className="col-md-10 col-lg-8 col-xl-7 mx-auto">
+              <form>
+                <div className="form-row">
+                  <div className="col-12 col-md-9 mb-2 mb-md-0">
+                    <input
+                      type="email"
+                      className="form-control form-control-lg"
+                      placeholder="Enter your email..."
+                    />
+                  </div>
+                  <div className="col-12 col-md-3">
+                    <button
+                      type="submit"
+                      className="btn btn-block btn-lg btn-primary"
+                    >
+                      Sign up!
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </header>
     </Layout>
   )
 }
