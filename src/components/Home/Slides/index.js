da3dddbf03bb59da0b01a36138917e11ca70a896
Leo <coder.leo.le@gmail.com>
2019-03-01 20:01:13 +0700

    Update images for Landing / About / News page

--- src/components/Home/Slides/index.js
+++ src/components/Home/Slides/index.js
@@ -3,43 +3,42 @@ import { withPrefix } from 'gatsby'
 import { Carousel } from 'react-bootstrap'
 import styles from './styles.module.scss'
 
+const data = [
+  {
+    imagePath: '/img/landing_carousel/example-1.jpg',
+    altText: 'Carousel',
+    title: '',
+    description: '',
+  },
+  {
+    imagePath: '/img/landing_carousel/example-2.jpg',
+    altText: 'Carousel',
+    title: '',
+    description: '',
+  },
+  {
+    imagePath: '/img/landing_carousel/example-3.jpg',
+    altText: 'Carousel',
+    title: '',
+    description: '',
+  },
+]
+
 const Slides = () => (
   <Carousel>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src={withPrefix('/img/landing_carousel/example-1.jpg')}
-        alt="First slide"
-      />
-      <Carousel.Caption>
-        <h3>First slide label</h3>
-        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src={withPrefix('/img/landing_carousel/example-2.png')}
-        alt="Third slide"
-      />
-
-      <Carousel.Caption>
-        <h3>Second slide label</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src={withPrefix('/img/landing_carousel/example-3.jpg')}
-        alt="Third slide"
-      />
-
-      <Carousel.Caption>
-        <h3>Third slide label</h3>
-        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
+    {data.map((item, index) => (
+      <Carousel.Item key={index}>
+        <img
+          className="d-block w-100"
+          src={withPrefix(item.imagePath)}
+          alt={item.altText}
+        />
+        <Carousel.Caption>
+          <h3>{item.title}</h3>
+          <p>{item.description}</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    ))}
   </Carousel>
 )
 
